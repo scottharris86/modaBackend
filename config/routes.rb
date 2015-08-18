8a838f7944b89bd9094c81c8423851205571c33d
Scott Harris <scottharris@outlook.com>
2015-08-18 11:24:37 -0400

    copy and paste errors

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,6 @@ Rails.application.routes.draw do
 
   delete 'favorites/:id' => 'favorites#destroy'
 
-  root
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
